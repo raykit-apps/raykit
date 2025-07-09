3c12d26d809d3fb8bcea4d1de85973089f86fac6
spuermomonga <1638829841@qq.com>
2025-07-09 15:32:42 +0800

    fix: Tauri adds tokio

--- crates/raykit-tauri/src/lib.rs
+++ crates/raykit-tauri/src/lib.rs
@@ -1,48 +1,48 @@
-use tauri::{path::BaseDirectory, Manager};
-use tauri_plugin_shell::{process::CommandEvent, ShellExt};
+// use tauri::{path::BaseDirectory, Manager};
+// use tauri_plugin_shell::{process::CommandEvent, ShellExt};
 
-mod core;
-mod system;
+pub mod core;
+pub mod system;
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+// #[tauri::command]
+// fn greet(name: &str) -> String {
+//     format!("Hello, {}! You've been greeted from Rust!", name)
+// }
 
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
-pub fn run() {
-    tauri::Builder::default()
-        .setup(|app| {
-            system::tray::create_tray(app.handle())?;
+// #[cfg_attr(mobile, tauri::mobile_entry_point)]
+// pub fn run() {
+//     tauri::Builder::default()
+//         .setup(|app| {
+//             system::tray::create_tray(app.handle())?;
 
-            let resource_path = app.path().resolve("exts-server.js", BaseDirectory::Resource)?;
-            println!("{:?}", resource_path.as_path());
-            let sidecar_command = app.shell().sidecar("node")?.arg(resource_path.as_path());
-            let (mut rx, child) = sidecar_command.spawn()?;
+//             let resource_path = app.path().resolve("exts-server.js", BaseDirectory::Resource)?;
+//             println!("{:?}", resource_path.as_path());
+//             let sidecar_command = app.shell().sidecar("node")?.arg(resource_path.as_path());
+//             let (mut rx, child) = sidecar_command.spawn()?;
 
-            println!("nodejs 进程ID{}", child.pid());
+//             println!("nodejs 进程ID{}", child.pid());
 
-            tauri::async_runtime::spawn(async move {
-                // read events such as stdout
-                while let Some(event) = rx.recv().await {
-                    if let CommandEvent::Stdout(line_bytes) = event {
-                        let line = String::from_utf8_lossy(&line_bytes);
-                        println!("{:?}", line);
-                    }
-                }
-            });
+//             tauri::async_runtime::spawn(async move {
+//                 // read events such as stdout
+//                 while let Some(event) = rx.recv().await {
+//                     if let CommandEvent::Stdout(line_bytes) = event {
+//                         let line = String::from_utf8_lossy(&line_bytes);
+//                         println!("{:?}", line);
+//                     }
+//                 }
+//             });
 
-            #[cfg(target_os = "macos")]
-            app.set_dock_visibility(false);
+//             #[cfg(target_os = "macos")]
+//             app.set_dock_visibility(false);
 
-            core::window::create(app.handle())?;
+//             core::window::create(app.handle())?;
 
-            Ok(())
-        })
-        .plugin(tauri_plugin_opener::init())
-        .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
-}
+//             Ok(())
+//         })
+//         .plugin(tauri_plugin_opener::init())
+//         .plugin(tauri_plugin_shell::init())
+//         .invoke_handler(tauri::generate_handler![greet])
+//         .run(tauri::generate_context!())
+//         .expect("error while running tauri application");
+// }
--- crates/raykit-tauri/src/main.rs
+++ crates/raykit-tauri/src/main.rs
@@ -1,6 +1,36 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::generate_context;
+
 fn main() {
-    raykit_tauri::run()
+    let tauri_context = generate_context!();
+
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async {
+            tauri::async_runtime::set(tokio::runtime::Handle::current());
+
+            let builder = tauri::Builder::default()
+                .setup(move |tauri_app| {
+                    raykit_tauri::core::window::create(tauri_app.handle())?;
+
+                    raykit_tauri::system::tray::create_tray(tauri_app.handle())?;
+
+                    Ok(())
+                })
+                .plugin(tauri_plugin_opener::init())
+                .plugin(tauri_plugin_shell::init())
+                .invoke_handler(tauri::generate_handler![]);
+
+            builder
+                .build(tauri_context)
+                .expect("Failed to build tauri app")
+                .run(|app_handle, event| {
+                    let _ = (app_handle, event);
+                });
+        });
+    // raykit_tauri::run()
 }
