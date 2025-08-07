7826d925b193353aeda26fe7e6a1ee4527219349
spuermomonga <1638829841@qq.com>
2025-08-07 22:11:59 +0800

    feat: The app adds log recording.

--- crates/raykit/src/main.rs
+++ crates/raykit/src/main.rs
@@ -1,12 +1,21 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::fs;
+
 use tauri::generate_context;
 
 use raykit::core;
+use tauri_plugin_log::{Target, TargetKind};
 
 fn main() {
     let tauri_context = generate_context!();
+
+    let config_dir = dirs::home_dir().expect("missing home dir").join(".raykit");
+    fs::create_dir(&config_dir).expect("failed to create config dir");
+
+    let extensions_dir = config_dir.join("extensions");
+    fs::create_dir(&extensions_dir).expect("failed to create extensions dir");
 
     tokio::runtime::Builder::new_multi_thread()
         .enable_all()
@@ -14,6 +23,13 @@ fn main() {
         .unwrap()
         .block_on(async {
             tauri::async_runtime::set(tokio::runtime::Handle::current());
+
+            // Configure the log output directory and level
+            let log = tauri_plugin_log::Builder::default()
+                .target(Target::new(TargetKind::LogDir {
+                    file_name: Some("ui-logs".to_string()),
+                }))
+                .level(log::LevelFilter::Error);
 
             let builder = tauri::Builder::default()
                 .setup(move |tauri_app| {
@@ -25,6 +41,7 @@ fn main() {
                 })
                 .plugin(tauri_plugin_opener::init())
                 .plugin(tauri_plugin_shell::init())
+                .plugin(log.build())
                 .invoke_handler(tauri::generate_handler![
                     core::commands::search_commands,
                     core::commands::execute_command
