490bc9f38763e9ba322f1d1d4a3298943c3ecc76
SpuerMomonga <1638829841@qq.com>
2026-03-05 09:54:16 +0800

    feat: enhance ApplicationShell class with initialization logic and maximized element

--- packages/core/src/browser/shell/application-shell.ts
+++ packages/core/src/browser/shell/application-shell.ts
@@ -1,5 +1,27 @@
+import type { DockPanel } from '@lumino/widgets'
 import { Widget } from '@raykit/widgets'
-import { injectable } from 'inversify'
+import { injectable, postConstruct } from 'inversify'
 
 @injectable()
-export class ApplicationShell extends Widget {}
+export class ApplicationShell extends Widget {
+  mainPanel?: DockPanel
+
+  protected readonly maximizedElement: HTMLElement
+
+  constructor() {
+    super()
+
+    this.maximizedElement = this.node.ownerDocument.createElement('div')
+    this.maximizedElement.className = 'fixed top-0 left-0 bottom-0 right-0 z-[2000]'
+    this.node.ownerDocument.body.appendChild(this.maximizedElement)
+  }
+
+  @postConstruct()
+  protected init() {
+    this.initializeShell()
+  }
+
+  protected initializeShell() {
+
+  }
+}
